5777b19383e5d1170964c087040851406db20560
McFlh4 <matthieu.roman2002@gmail.com>
2022-04-01 15:13:49 +0200

    Add Dico for crytpos

---
+++ cryptoDict.py
@@ -0,0 +1,37 @@
+coinIdDict = {
+    'Bitcoin' : 'bitcoin',
+    'Ethereum' : 'ethereum',
+    'Litecoin' : 'litecoin',
+    'Dogecoin' : 'dogecoin',
+    'Bitcoin Cash' : 'bitcoin-cash',
+    'Cardano' : 'cardano',
+    'Matic Network' : 'matic-network',
+    'Decentraland [MANA]' : 'decentraland',
+    'Gala' : 'gala',
+    'MBOX' : 'mobox',
+    'Avalanche': 'avalanche-2',
+    'Polkadot' : 'polkadot',
+    'Solana' : 'solana IAN FAUT INVESTIR CA VA PETER LA',
+    'Terra Luna' : 'terra-luna',
+    'Basic Attention Token' : 'bat',
+}
+
+moneyIdDict = {
+    'US Dollars' : 'usd',
+    'Indian Rupees' : 'inr',
+    'Euro' : 'eur',
+    'British Pound' : 'gbp',
+    'Japanese Yen' : 'jpy',
+    'Canadian Dollar' : 'cad',
+    'Australian Dollar' : 'aud'
+}
+
+moneySymbolDict = {
+    'usd' : '$',
+    'inr' : '₹',
+    'eur' : '€',
+    'gbp' : '£',
+    'jpy' : '¥',
+    'cad' : 'C$',
+    'aud' : 'A$'
+}
